c807bb3c7f2984ac743e7dab38afa5fc7a2e8b7b
diacritics-owo <176600453+diacritics-owo@users.noreply.github.com>
2024-09-24 10:25:54 +0530

    initial commit

---
+++ src/client/java/diacritics/owo/MCAUtilitiesClient.java
@@ -0,0 +1,8 @@
+package diacritics.owo;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MCAUtilitiesClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {}
+}
---
+++ src/main/java/diacritics/owo/MCAUtilities.java
@@ -0,0 +1,13 @@
+package diacritics.owo;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MCAUtilities implements ModInitializer {
+	public static final String MOD_ID = "mca-utilities";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {}
+}
